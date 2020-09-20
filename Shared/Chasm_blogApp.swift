3f3bbf4ae4c822e2a6f07304bae8e1e1429fa479
Martin Hartl <martin@hartl.co>
2020-09-20 07:05:00 +0200

    Initial Commit

---
+++ Shared/Chasm_blogApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chasm_blogApp.swift
+//  Shared
+//
+//  Created by Martin Hartl on 20.09.20.
+//
+
+import SwiftUI
+
+@main
+struct Chasm_blogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
